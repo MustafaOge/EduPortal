b8aff2b80045887bbccf90aff9f8209168fbace4
mustafa <mustafaoge1221@gmail.com>
2024-06-27 09:28:30 +0300

    Code-Review

--- EduPortal.MVC/Controllers/OutageNotificationController.cs
+++ EduPortal.MVC/Controllers/OutageNotificationController.cs
@@ -31,7 +31,7 @@ namespace EduPortal.MVC.Controllers
         {
             if (!DateTime.TryParse(selectedDate, out DateTime date))
             {
-                return BadRequest("Geçersiz tarih formatı.");
+                return BadRequest("Geçersiz tarih formatı girildi.");
             }
 
             var outages = _outageNotificationService.GetOutagesByDateAndDistrict(date, district);
